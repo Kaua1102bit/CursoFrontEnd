717dc1d98a4f95eb8b72f13153ae627a02d68093
Kaua1102bit <kauarsfavika@gmail.com>
2025-03-06 11:26:46 -0300

    06.03.25

--- DOM/Exercicio1DOM/script.js
+++ DOM/Exercicio1DOM/script.js
@@ -1,9 +1,20 @@
 //exercicio 2 - a
 function alterarTexto() {
-    document.getElementById('titulo').innerText = 'Cadastro Realizado';
-    document.querySelector('.paragrafo').innerText = 'Obrigado por se cadastrar!';
-}
+    let titulo = document.getElementById('titulo');
+    titulo.innerText = 'Cadastro Realizado';
 
+    let paragrafo = document.querySelector('.paragrafo');
+    paragrafo.innerText = 'Obrigado por se cadastrar!';
+
+}
+// 3 - a
 function alterarCorFundo(){
    document.body.style.backgroundColor = "Blue";
+}
+
+// 4 - a
+function adicionarClasse(){
+    let titulo = document.getElementById('titulo');
+    titulo.classList.add('descricao');
+    titulo.style.color = "red"; // Adiciona a cor vermelha ao texto do título
 }
